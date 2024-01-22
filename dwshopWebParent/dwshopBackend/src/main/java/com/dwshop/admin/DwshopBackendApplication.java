8aabe89b6a6c9f4a045dca0d1ae3457c1043aef5
ShiroganeIXV <wudiana.124010121105@vtc.edu.vn>
2024-01-22 13:50:13 +0700

    Create Project Outline

---
+++ dwshopWebParent/dwshopBackend/src/main/java/com/dwshop/admin/DwshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dwshop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwshopBackendApplication.class, args);
+	}
+
+}
---
+++ dwshopWebParent/dwshopFrontend/src/main/java/com/dwshop/site/DwshopFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.dwshop.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwshopFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwshopFrontendApplication.class, args);
+	}
+
+}
