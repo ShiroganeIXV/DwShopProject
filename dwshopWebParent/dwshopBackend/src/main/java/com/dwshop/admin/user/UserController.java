1150cbec21b659347f93eef41743462907669cd8
ShiroganeIXV <wudiana.124010121105@vtc.edu.vn>
2024-01-24 10:01:02 +0700

    Create NewUserForm

--- dwshopWebParent/dwshopBackend/src/main/java/com/dwshop/admin/user/UserController.java
+++ dwshopWebParent/dwshopBackend/src/main/java/com/dwshop/admin/user/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -14,11 +15,29 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    // list all users
     @GetMapping("/users")
     public String listAll(Model model) {
         List<User> listUsers = service.listAll();
         model.addAttribute("listUsers", listUsers);
         return "users";
     }
+
+    // add new user
+    @GetMapping("/users/new")
+    public String newUser(Model model){
+        User user = new User();
+        // bind USER object with model attribute
+        model.addAttribute("user", user);
+        return "user_form";
+    }
+        // save user
+    @PostMapping("/users/save")
+    public String saveUser(User user){
+        System.out.println(user);
+        return "redirect:/users";
+    }
+
+
 
 }
