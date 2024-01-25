82dfca67b4eb0f0a67ff776b84bb2287ab78bf9b
ShiroganeIXV <wudiana.124010121105@vtc.edu.vn>
2024-01-25 20:57:06 +0700

    Add PasswordEncoder

--- dwshopWebParent/dwshopBackend/src/main/java/com/dwshop/admin/user/UserService.java
+++ dwshopWebParent/dwshopBackend/src/main/java/com/dwshop/admin/user/UserService.java
@@ -3,6 +3,7 @@ package com.dwshop.admin.user;
 import com.dwshop.common.entity.Role;
 import com.dwshop.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +16,10 @@ public class UserService {
     @Autowired
     private RoleRepository roleRepo;
 
+    // PasswordEncoder is a Spring Security interface for encrypting passwords.
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public List<User> listAll() {
         List<User> listUsers = (List<User>) userRepo.findAll();
         return listUsers;
@@ -25,6 +30,14 @@ public class UserService {
     }
 
     public void save(User user) {
+        encodePassword(user);
         userRepo.save(user);
     }
+
+    // private Methods
+    private void encodePassword(User user){
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+    }
+
 }
